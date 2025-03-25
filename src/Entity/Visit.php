9d19f76ea5ad3948b41e073c35f2ef4161551c90
Justin Cornell <justin@cornifex.com>
2025-03-24 19:46:34 -0500

    Visits entity

---
+++ src/Entity/Visit.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\link_tracker\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\link_tracker\VisitInterface;
+
+/**
+ * Defines the visit entity class.
+ *
+ * @ContentEntityType(
+ *   id = "link_tracker_visit",
+ *   label = @Translation("Visit"),
+ *   label_collection = @Translation("Visits"),
+ *   label_singular = @Translation("visit"),
+ *   label_plural = @Translation("visits"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count visits",
+ *     plural = "@count visits",
+ *   ),
+ *   base_table = "link_tracker_visit",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "id",
+ *     "uuid" = "uuid",
+ *   },
+ * )
+ */
+final class Visit extends ContentEntityBase implements VisitInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) : array {
+    $fields = parent::baseFieldDefinitions($entity_type);
+    $fields['ip_address'] = BaseFieldDefinition::create('string');
+    $fields['count'] = BaseFieldDefinition::create('integer');
+    return $fields;
+  }
+
+}
---
+++ src/VisitInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\link_tracker;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining a visit entity type.
+ */
+interface VisitInterface extends ContentEntityInterface {
+
+}
